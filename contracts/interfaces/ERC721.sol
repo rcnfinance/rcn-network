3bf38d1f6701116126f962cdf26304203c902805
Agustin Aguilar <agustin@bitpagos.co>
2018-02-22 13:08:10 -0300

    Engine and ERC721 interfaces

---
+++ contracts/interfaces/ERC721.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.15;
+
+contract ERC721 {
+   // ERC20 compatible functions
+   function name() constant returns (string _name);
+   function symbol() constant returns (string _symbol);
+   function totalSupply() constant returns (uint256 _totalSupply);
+   function balanceOf(address _owner) constant returns (uint _balance);
+   // Functions that define ownership
+   function ownerOf(uint256) constant returns (address owner);
+   function approve(address, uint256) public returns (bool);
+   function takeOwnership(uint256) public returns (bool);
+   function transfer(address, uint256) public returns (bool);
+   // Events
+   event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+   event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
+}
---
+++ contracts/interfaces/Engine.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.15;
+
+import './Oracle.sol';
+
+contract Engine {
+    enum Status { initial, lent, paid, destroyed }
+    struct Approbation {
+        bool approved;
+        bytes data;
+        bytes32 checksum;
+    }
+
+    function getTotalLoans() constant returns (uint256);
+    function getOracle(uint index) constant returns (Oracle);
+    function getBorrower(uint index) constant returns (address);
+    function getCosigner(uint index) constant returns (address);
+    function ownerOf(uint256) constant returns (address owner);
+    function getCreator(uint index) constant returns (address);
+    function getAmount(uint index) constant returns (uint256);
+    function getPaid(uint index) constant returns (uint256);
+    function getDueTime(uint index) constant returns (uint256);
+    function getApprobation(uint index, address _address) constant returns (bool);
+    function getStatus(uint index) constant returns (Status);
+    function isApproved(uint index) constant returns (bool);
+    function getPendingAmount(uint index) constant public returns (uint256);
+    function getCurrency(uint index) constant public returns (bytes32);
+
+    function approveLoan(uint index) public returns (bool);
+    function transfer(address to, uint256 index) public returns (bool);
+    function takeOwnership(uint256 index) public returns (bool);
+    function withdrawal(uint index, address to, uint256 amount) public returns (bool);
+}
