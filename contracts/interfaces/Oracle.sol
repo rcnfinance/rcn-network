25faed6bbc36914f01a9f62987b574d091b4f653
Agustin Aguilar <agustin@bitpagos.co>
2018-02-22 12:58:09 -0300

    Oracle V2, more flexible and with off-chain possibilities

--- contracts/interfaces/Oracle.sol
+++ contracts/interfaces/Oracle.sol
@@ -1,9 +1,36 @@
 pragma solidity ^0.4.15;
 
-contract Oracle {
-    event NewSymbol(string _symbol, uint8 _decimals);
-    function getTimestamp(string symbol) constant returns(uint256);
-    function getRateFor(string symbol) returns (uint256);
-    function getCost(string symbol) constant returns (uint256);
-    function getDecimals(string symbol) constant returns (uint256);
+import "./../utils/Ownable.sol";
+import "./Token.sol";
+
+contract Oracle is Ownable {
+    uint256 public constant VERSION = 2;
+
+    event NewSymbol(bytes32 _currency, string _ticker, uint8 _decimals);
+    
+    struct Symbol {
+        string ticker;
+        uint8 decimals;
+        bool supported;
+    }
+
+    mapping(bytes32 => Symbol) public currencies;
+
+    function url() constant returns (string);
+    function getRate(bytes32 symbol, bytes data) constant returns (uint256);
+
+    function addCurrency(string ticker, uint8 decimals) public onlyOwner returns (bytes32) {
+        NewSymbol(currency, ticker, decimals);
+        bytes32 currency = keccak256(ticker, decimals);
+        currencies[currency] = Symbol(ticker, decimals, true);
+        return currency;
+    }
+
+    function decimals(bytes32 symbol) constant returns (uint8) {
+        return currencies[symbol].decimals;
+    }
+
+    function supported(bytes32 symbol) constant returns (bool) {
+        return currencies[symbol].supported;
+    }
 }
