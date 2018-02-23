d8ce66bbb6398655eae7cb8e458a4e19774d51f1
Agustin Aguilar <agustin@bitpagos.co>
2018-02-23 16:17:11 -0300

    Implemented cache to prevent shorting when using the oracle

--- contracts/examples/ReferenceOracle.sol
+++ contracts/examples/ReferenceOracle.sol
@@ -9,13 +9,20 @@ import './../interfaces/Oracle.sol';
 contract ReferenceOracle is Oracle, Delegable, BytesUtils {
     uint256 public expiration = 15 minutes;
 
-    uint constant private INDEX_RATE = 0;
-    uint constant private INDEX_TIMESTAMP = 1;
+    uint constant private INDEX_TIMESTAMP = 0;
+    uint constant private INDEX_RATE = 1;
     uint constant private INDEX_V = 2;
     uint constant private INDEX_R = 3;
     uint constant private INDEX_S = 4;
 
     string private infoUrl;
+
+    mapping(bytes32 => RateCache) private cache;
+
+    struct RateCache {
+        uint256 timestamp;
+        uint256 rate;
+    }
 
     function url() public constant returns (string) {
         return infoUrl;
@@ -32,19 +39,26 @@ contract ReferenceOracle is Oracle, Delegable, BytesUtils {
     }
 
     function getRate(bytes32 currency, bytes data) constant returns (uint256) {
-        uint256 rate = uint256(readBytes32(data, INDEX_RATE));
         uint256 timestamp = uint256(readBytes32(data, INDEX_TIMESTAMP));
-        uint8 v = uint8(readBytes32(data, INDEX_V));
-        bytes32 r = readBytes32(data, INDEX_R);
-        bytes32 s = readBytes32(data, INDEX_S);
-        
-        bytes32 hash = keccak256(this, currency, rate, timestamp);
-        address signer = ecrecover(keccak256("\x19Ethereum Signed Message:\n32", hash),v,r,s);
-
-        require(isDelegate(signer));
-        require(timestamp > block.timestamp - expiration);
         require(timestamp <= block.timestamp);
+        require(timestamp > block.timestamp - expiration);
 
-        return rate;
+        if (cache[currency].timestamp >= timestamp) {
+            return cache[currency].rate;
+        } else {
+            uint256 rate = uint256(readBytes32(data, INDEX_RATE));
+            uint8 v = uint8(readBytes32(data, INDEX_V));
+            bytes32 r = readBytes32(data, INDEX_R);
+            bytes32 s = readBytes32(data, INDEX_S);
+            
+            bytes32 hash = keccak256(this, currency, rate, timestamp);
+            address signer = ecrecover(keccak256("\x19Ethereum Signed Message:\n32", hash),v,r,s);
+
+            require(isDelegate(signer));
+
+            cache[currency] = RateCache(timestamp, rate);
+
+            return rate;
+        }
     }
 }   
