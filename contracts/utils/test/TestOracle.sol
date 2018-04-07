3860fea5c453804f5df0005e70cfc1bce33f57e0
Agustin Aguilar <agusxrun@gmail.com>
2018-04-07 12:44:08 -0300

    Test new oracle decimals

--- contracts/utils/test/TestOracle.sol
+++ contracts/utils/test/TestOracle.sol
@@ -4,8 +4,11 @@ import "./../../interfaces/Oracle.sol";
 import "./../../utils/BytesUtils.sol";
 
 contract TestOracle is Oracle, BytesUtils {
-    bytes32 public dummyData = keccak256("test_oracle");
-    bytes public dummyDataBytes = buildData(dummyData);
+    bytes32 public dummyData1 = keccak256("test_oracle_1");
+    bytes32 public dummyData2 = keccak256("test_oracle_2");
+    
+    bytes public dummyDataBytes1 = buildData(dummyData1);
+    bytes public dummyDataBytes2 = buildData(dummyData2);
 
     function buildData(bytes32 d) internal pure returns (bytes o) {
         assembly {
@@ -21,8 +24,13 @@ contract TestOracle is Oracle, BytesUtils {
         return "";
     }
 
-    function getRate(bytes32, bytes data) public returns (uint256) {
-        require(readBytes32(data, 0) == dummyData);
-        return 6000;
+    function getRate(bytes32, bytes data) public returns (uint256 rate, uint256 decimals) {
+        bytes32 sentData = readBytes32(data, 0);
+        require(sentData == dummyData1 || sentData == dummyData2);
+        if (sentData == dummyData1) {
+            return (6000, 18);
+        } else if (sentData == dummyData2) {
+            return (5, 17);
+        }
     }
 }
