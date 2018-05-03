099870a400429c91c2720289fcf35df640efc704
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-05-03 18:08:32 -0300

    Fix BytesUtils.sol test

--- test/BytesUtilsTest.sol
+++ test/BytesUtilsTest.sol
@@ -4,8 +4,12 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/utils/BytesUtils.sol";
 
+interface BytesUtilsInterface {
+    function pReadBytes32(bytes data, uint256 index) public;
+}
+
 contract TestBytesUtils is BytesUtils {
-    function pReadBytes32(bytes data, uint256 index) public pure returns (bytes32) {
+    function pReadBytes32(bytes data, uint256 index) public returns (bytes32) {
         return readBytes32(data, index);
     }
 }
@@ -15,16 +19,16 @@ contract ThrowProxy {
     address public target;
     bytes data;
 
-    function ThrowProxy(address _target) {
+    function ThrowProxy(address _target) public {
         target = _target;
     }
 
     //prime the data using the fallback function.
-    function() {
+    function() public {
         data = msg.data;
     }
 
-    function execute() returns (bool) {
+    function execute() public returns (bool) {
         return target.call(data);
     }
 }
@@ -55,7 +59,7 @@ contract BytesUtilsTest {
         }
     }
 
-    function testReadBytes() {
+    function testReadBytes() public {
         TestBytesUtils bytesUtils = new TestBytesUtils();
         bytes memory testData = buildData(bytes32(uint256(123)), bytes32(address(this)), keccak256("test"), bytes32(0x789));
         Assert.equal(bytesUtils.pReadBytes32(testData, 0), bytes32(uint256(123)), "Read index 0, uint256");
@@ -64,21 +68,21 @@ contract BytesUtilsTest {
         Assert.equal(bytesUtils.pReadBytes32(testData, 3), bytes32(0x789), "Read index 3, bytes32");
     }
 
-    function testReadNonBytesMemory() {
+    function testReadNonBytesMemory() public {
         TestBytesUtils bytesUtils = new TestBytesUtils();
         ThrowProxy throwProxy = new ThrowProxy(address(bytesUtils));
         bytes memory testData = buildData(bytes32(uint256(123)), bytes32(address(this)), keccak256("test"), bytes32(0x789));
 
         // Test read index 4 (invalid)
-        TestBytesUtils(address(throwProxy)).pReadBytes32(testData, 4);
+        BytesUtilsInterface(address(throwProxy)).pReadBytes32(testData, 4);
         Assert.isFalse(throwProxy.execute.gas(200000)(), "Read index 4 should fail, it's not inside bytes array");
 
         // Test read index 0 of empty bytes (invalid)
-        TestBytesUtils(address(throwProxy)).pReadBytes32(new bytes(0), 0);
+        BytesUtilsInterface(address(throwProxy)).pReadBytes32(new bytes(0), 0);
         Assert.isFalse(throwProxy.execute.gas(200000)(), "Read index 0 should fail, it's not inside bytes array");
     }
 
-    function testInvalidLengthBytes() {
+    function testInvalidLengthBytes() public {
         TestBytesUtils bytesUtils = new TestBytesUtils();
         ThrowProxy throwProxy = new ThrowProxy(address(bytesUtils));
         bytes memory testData = buildData(bytes32(uint256(123)), bytes32(address(this)), bytes4(keccak256("test")));
@@ -88,7 +92,7 @@ contract BytesUtilsTest {
         Assert.equal(bytesUtils.pReadBytes32(testData, 1), bytes32(address(this)), "Read index 1, address");
 
         // Reading index 2 should fail, the word has less than 32 bytes
-        TestBytesUtils(address(throwProxy)).pReadBytes32(testData, 2);
+        BytesUtilsInterface(address(throwProxy)).pReadBytes32(testData, 2);
         Assert.isFalse(throwProxy.execute.gas(200000)(), "Reading index 3 should fail, the word has less than 32 bytes");
     }
 }
