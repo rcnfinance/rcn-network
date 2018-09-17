e640967c7b241e659abe01a0dee0e62863c43bcd
Joaquin Pablo Gonzalez <joaquin@ripiocredit.network>
2018-09-17 15:03:51 -0300

    Optimized gas cost.

--- contracts/test-helper/SafeMathWrapperMock.sol
+++ contracts/test-helper/SafeMathWrapperMock.sol
@@ -6,36 +6,36 @@ import "../interfaces/LoanStatus.sol";
 contract SafeMathWrapperMock {
 
     function mul(uint256 a, uint256 b) public pure returns (bool) {
-        SafeMathWrapper.Result memory result = SafeMathWrapper.Result(LoanStatus.Status.unknown, "metadata");
-        SafeMathWrapper.safeMul(a, b, result);
-        return result.status != LoanStatus.Status.error;
+        SafeMathWrapper.Data memory data = SafeMathWrapper.Data(0);
+        SafeMathWrapper.safeMul(a, b, data);
+        return data.status != uint256(LoanStatus.Status.error);
     }
 
     function div(uint256 a, uint256 b) public pure returns (bool) {
-        SafeMathWrapper.Result memory result = SafeMathWrapper.Result(LoanStatus.Status.unknown, "metadata");
-        SafeMathWrapper.safeDiv(a, b, result);
-        return result.status != LoanStatus.Status.error;
+        SafeMathWrapper.Data memory data = SafeMathWrapper.Data(0);
+        SafeMathWrapper.safeDiv(a, b, data);
+        return data.status != uint256(LoanStatus.Status.error);
     }
 
     function sub(uint256 a, uint256 b) public pure returns (bool) {
-        SafeMathWrapper.Result memory result = SafeMathWrapper.Result(LoanStatus.Status.unknown, "metadata");
-        SafeMathWrapper.safeSub(a, b, result);
-        return result.status != LoanStatus.Status.error;
+        SafeMathWrapper.Data memory data = SafeMathWrapper.Data(0);
+        SafeMathWrapper.safeSub(a, b, data);
+        return data.status != uint256(LoanStatus.Status.error);
     }
 
     function add(uint256 a, uint256 b) public pure returns (bool) {
-        SafeMathWrapper.Result memory result = SafeMathWrapper.Result(LoanStatus.Status.unknown, "metadata");
-        SafeMathWrapper.safeAdd(a, b, result);
-        return result.status != LoanStatus.Status.error;
+        SafeMathWrapper.Data memory data = SafeMathWrapper.Data(0);
+        SafeMathWrapper.safeAdd(a, b, data);
+        return data.status != uint256(LoanStatus.Status.error);
     }
 
     function chainOperations(uint256 a, uint256 b) public pure returns (bool) {
-        SafeMathWrapper.Result memory result = SafeMathWrapper.Result(LoanStatus.Status.unknown, "metadata");
-        SafeMathWrapper.safeMul(a, b, result);
-        SafeMathWrapper.safeAdd(a, b, result);
-        SafeMathWrapper.safeDiv(a, b, result);
-        SafeMathWrapper.safeSub(a, b, result);
-        return result.status != LoanStatus.Status.error;
+        SafeMathWrapper.Data memory data = SafeMathWrapper.Data(0);
+        SafeMathWrapper.safeMul(a, b, data);
+        SafeMathWrapper.safeAdd(a, b, data);
+        SafeMathWrapper.safeDiv(a, b, data);
+        SafeMathWrapper.safeSub(a, b, data);
+        return data.status != uint256(LoanStatus.Status.error);
     }
 
 }
--- contracts/utils/SafeMathWrapper.sol
+++ contracts/utils/SafeMathWrapper.sol
@@ -4,46 +4,43 @@ import "../interfaces/LoanStatus.sol";
 
 library SafeMathWrapper {
 
-    struct Result { 
-        LoanStatus.Status status;
-        string metadata; // metadata for events 
-    }
+    struct Data {
+        uint256 status;
+    } 
 
     /**
     * @dev Adds two number, returns an error status on overflow.
     */
-    function safeAdd(uint256 a, uint256 b, Result memory self) internal pure returns (uint256) {
-        if (self.status == LoanStatus.Status.error) {
+    function safeAdd(uint256 a, uint256 b, Data memory self) internal pure returns (uint256) {
+        if (self.status == uint8(LoanStatus.Status.error)) {
             return;
         }  
         uint256 c = a + b;
         if (c >= a) {
             return c;
         }  
-        self.status = LoanStatus.Status.error;
-        self.metadata = "Add overflow";
+        self.status = uint256(LoanStatus.Status.error); 
     }
 
     /**
-    * @dev Subtracts two numbers, returns an error status on overflow..
+    * @dev Subtracts two numbers, returns an error status on overflow.
     */
-    function safeSub(uint256 x, uint256 y, Result memory self) internal pure returns (uint256) {
-        if (self.status == LoanStatus.Status.error) {
+    function safeSub(uint256 a, uint256 b, Data memory self) internal pure returns (uint256) {
+        if (self.status == uint256(LoanStatus.Status.error)) {
             return;
         } 
-        if (x >= y) {
-            uint256 z = x - y;
-            return z;
+        if (a >= b) { 
+            uint256 z = a - b;
+            return z; 
         }
-        self.status = LoanStatus.Status.error;
-        self.metadata = "Sub underflow";
+        self.status = uint256(LoanStatus.Status.error); 
     }
 
     /**
     * @dev Multiplies two numbers, returns an error status on overflow.
     */
-    function safeMul(uint256 a, uint256 b, Result memory self) internal pure returns (uint256) {
-        if (self.status == LoanStatus.Status.error) {
+    function safeMul(uint256 a, uint256 b, Data memory self) internal pure returns (uint256) {
+        if (self.status == uint256(LoanStatus.Status.error)) {
             return;
         } 
         if (a == 0) {
@@ -53,24 +50,22 @@ library SafeMathWrapper {
         if (c / a == b) {
             return c;
         }
-        self.status = LoanStatus.Status.error;
-        self.metadata = "Mult overflow";
+        self.status = uint256(LoanStatus.Status.error); 
 
     }
   
     /**
     * @dev Integer division of two numbers truncating the quotient, returns an error status on overflow.
     */
-    function safeDiv(uint256 a, uint256 b, Result memory self) internal pure returns (uint256) {
-        if (self.status == LoanStatus.Status.error) {
+    function safeDiv(uint256 a, uint256 b, Data memory self) internal pure returns (uint256) {
+        if (self.status == uint256(LoanStatus.Status.error)) {
             return;
         } 
         if (b > 0) { 
             uint256 c = a / b;
             return c;
         }
-        self.status = LoanStatus.Status.error;
-        self.metadata = "Div underflow";
+        self.status = uint256(LoanStatus.Status.error);  
     }
 
 }
