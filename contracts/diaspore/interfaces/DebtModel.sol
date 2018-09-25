39aed72f4ff78d924b8937f44b1d2f1bd744dba2
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-09-25 15:29:44 +0200

    Removed clock related methods

--- contracts/diaspore/interfaces/DebtModel.sol
+++ contracts/diaspore/interfaces/DebtModel.sol
@@ -10,27 +10,29 @@ contract DebtModel is ERC165 {
     event ChangedStatus(bytes32 indexed _id, uint256 _status);
     event ChangedDueTime(bytes32 indexed _id, uint64 _dueTime);
 
-    bytes4 internal debtModelInterface = this.validate.selector
-                                ^ this.getStatus.selector
-                                ^ this.getPaid.selector
-                                ^ this.getDebt.selector
-                                ^ this.getClock.selector
-                                ^ this.getDueTime.selector
-                                ^ this.create.selector
-                                ^ this.addPaid.selector
-                                ^ this.advanceClock.selector;
+    // Debt model interface selector
+    bytes4 internal debtModelInterface = 
+    this.validate.selector
+    ^ this.getStatus.selector
+    ^ this.getPaid.selector
+    ^ this.getDebt.selector
+    ^ this.getDueTime.selector
+    ^ this.create.selector
+    ^ this.addPaid.selector;
+
+    uint256 public constant STATUS_ONGOING = 1;
+    uint256 public constant STATUS_PAID = 2;
 
     // Meta
-    function owner() external returns (address);
+    function isOperator(address operator) external view returns (bool);
     function validate(bytes32[] loanData) external view returns (bool);
     // Getters
     function getStatus(bytes32 id) external view returns (uint256);
     function getPaid(bytes32 id) external view returns (uint256);
     function getDebt(bytes32 id) external view returns (uint256);
-    function getClock(bytes32 id) external view returns (uint256);
     function getDueTime(bytes32 id) external view returns (uint256);
     // Interface
     function create(bytes32 id, bytes32[] loanData) external returns (bool);
-    function addPaid(bytes32 id, uint256 target) external returns (uint256 real);
-    function advanceClock(bytes32 id, uint256 to) external returns (bool);
+    function addPaid(bytes32 id, uint256 amount) external returns (uint256 real);
+    function run(bytes32 id) external returns (bool);
 }
