bba6eea65898fed250904d2eb93bc7c515243cf2
Agustin Esteban Aguilar <agustin@bitpagos.co>
2018-09-27 15:44:33 +0200

    Add pay and payToken methods

--- contracts/diaspore/DebtEngine.sol
+++ contracts/diaspore/DebtEngine.sol
@@ -1,12 +1,18 @@
 pragma solidity ^0.4.24;
 
 import "./../interfaces/Token.sol";
-import "./../interfaces/Oracle.sol";
 import "./../interfaces/Cosigner.sol";
 import "./../utils/ERC721Base.sol";
 import "./interfaces/Model.sol";
 
+interface IOracle {
+    function getRate(bytes32 symbol, bytes data) external returns (uint256 rate, uint256 decimals);
+}
+
 contract DebtEngine is ERC721Base {
+    uint256 constant internal TOKEN_DECIMALS = 18;
+    uint256 constant internal PRECISION = 10 ** TOKEN_DECIMALS;
+
     Token public token;
 
     mapping(bytes32 => Debt) public debts;
@@ -88,17 +94,106 @@ contract DebtEngine is ERC721Base {
         return keccak256(abi.encodePacked(creator, nonce, method2));
     }
 
-    function pay(bytes32 id, uint256 amount, bytes oracleData) external returns (uint256 paid) {
+    function pay(
+        bytes32 id,
+        uint256 amount,
+        address origin,
+        bytes oracleData
+    ) external returns (uint256 paid, uint256 paidToken) {
         Debt storage debt = debts[id];
 
         // Paid only required amount
         paid = debt.model.addPaid(id, amount);
+        require(paid <= amount, "Paid can't be more than requested");
 
-        // TODO: Convert from currency to tokens
+        IOracle oracle = IOracle(debt.oracle);
+        if (oracle != address(0)) {
+            // Convert
+            (uint256 rate, uint256 decimals) = oracle.getRate(debt.currency, oracleData);
+            paidToken = toToken(paid, rate, decimals);
+        } else {
+            paidToken = paid;
+        }
 
         // Pull tokens from payer
-        require(token.transferFrom(msg.sender, address(this), paid), "Error pulling payment tokens");
-        debt.balance += uint128(paid);
+        require(token.transferFrom(msg.sender, address(this), paidToken), "Error pulling payment tokens");
+
+        // Add balance to the debt
+        uint256 newBalance = paidToken.add(debt.balance);
+        require(newBalance < 340282366920938463463374607431768211456, "uin128 Overflow");
+        debt.balance = uint128(newBalance);
+    }
+
+    function payToken(
+        bytes32 id,
+        uint256 amount,
+        address origin,
+        bytes oracleData
+    ) external returns (uint256 paid, uint256 paidToken) {
+        Debt storage debt = debts[id];
+
+        // Read storage
+        IOracle oracle = IOracle(debt.oracle);
+
+        uint256 rate;
+        uint256 decimals;
+        uint256 available;
+
+        // Get available <currency> amount
+        if (oracle != address(0)) {
+            (rate, decimals) = oracle.getRate(debt.currency, oracleData);
+            available = fromToken(amount, rate, decimals);
+        } else {
+            available = amount;
+        }
+
+        // Pay the debt
+        paid = debt.model.addPaid(id, available);
+        require(paid <= available, "Paid can't exceed available");
+
+        // Convert back to required pull amount
+        if (oracle != address(0)) {
+            paidToken = toToken(paid, rate, decimals);
+            require(paidToken <= amount, "Paid can't exceed requested");
+        } else {
+            paidToken = paid;
+        }
+
+        // Pull tokens from payer
+        require(token.transferFrom(msg.sender, address(this), paidToken), "Error pulling tokens");
+
+        // Add balance to the debt
+        uint256 newBalance = paidToken.add(debt.balance);
+        require(newBalance < 340282366920938463463374607431768211456, "uin128 Overflow");
+        debt.balance = uint128(newBalance);
+    }
+
+    /**
+        Converts an amount in the rate currency to an amount in token
+
+        @param amount Amount to convert in rate currency
+        @param rate Rate to use in the convertion
+        @param decimals Base difference between rate and tokens
+
+        @return Amount in tokens
+    */
+    function toToken(uint256 amount, uint256 rate, uint256 decimals) internal pure returns (uint256) {
+        require(decimals <= TOKEN_DECIMALS, "Decimals limit reached");
+        return rate.mult(amount).mult((10 ** (TOKEN_DECIMALS - decimals))) / PRECISION;
+    }
+
+    /**
+        Converts an amount in token to the rate currency
+
+        @param amount Amount to convert in token
+        @param rate Rate to use in the convertion
+        @param decimals Base difference between rate and tokens
+
+        @return Amount in rate currency
+    */
+    function fromToken(uint256 amount, uint256 rate, uint256 decimals) internal pure returns (uint256) {
+        require(decimals <= TOKEN_DECIMALS, "Decimals limit reached");
+        return amount.mult((10 ** (TOKEN_DECIMALS - decimals))) / (rate.mult(PRECISION));
     }
 
     function withdrawal(bytes32 id, address to) external returns (uint256 amount) {
