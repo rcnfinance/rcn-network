39c0f43cbf94888561024e611a9950c9c5f4487a
Agustin Aguilar <agustin@ripiocredit.network>
2018-10-17 00:04:49 +0200

    Read packed BytesUtils

--- contracts/utils/BytesUtils.sol
+++ contracts/utils/BytesUtils.sol
@@ -2,9 +2,17 @@ pragma solidity ^0.4.19;
 
 contract BytesUtils {
     function readBytes32(bytes data, uint256 index) internal pure returns (bytes32 o) {
-        require(data.length / 32 > index);
+        require(data.length / 32 > index, "Reading bytes out of bounds");
         assembly {
             o := mload(add(data, add(32, mul(32, index))))
+        }
+    }
+    function read(bytes data, uint256 offset, uint256 length) internal pure returns (bytes32 o) {
+        require(data.length >= offset + length, "Reading bytes out of bounds");
+        assembly {
+            o := mload(add(data, add(32, offset)))
+            let lb := sub(32, length)
+            if lb { o := div(o, exp(2, mul(lb, 8))) }
         }
     }
 }
--- test/BytesUtilsTest.sol
+++ test/BytesUtilsTest.sol
@@ -12,6 +12,10 @@ contract TestBytesUtils is BytesUtils {
     function pReadBytes32(bytes data, uint256 index) public returns (bytes32) {
         return readBytes32(data, index);
     }
+
+    function pRead(bytes data, uint256 offset, uint256 length) public returns (bytes32) {
+        return read(data, offset, length);
+    }
 }
 
 // Proxy contract for testing throws
@@ -34,6 +38,12 @@ contract ThrowProxy {
 }
 
 contract BytesUtilsTest {
+    TestBytesUtils bytesUtils;
+
+    function beforeAll() external {
+        bytesUtils = new TestBytesUtils();
+    }
+
     function buildData(bytes32 a, bytes32 b, bytes32 c, bytes32 d) internal returns (bytes o) {
         assembly {
             let size := 128
@@ -59,8 +69,7 @@ contract BytesUtilsTest {
         }
     }
 
-    function testReadBytes() public {
-        TestBytesUtils bytesUtils = new TestBytesUtils();
+    function testReadBytes() external {
         bytes memory testData = buildData(bytes32(uint256(123)), bytes32(address(this)), keccak256("test"), bytes32(0x789));
         Assert.equal(bytesUtils.pReadBytes32(testData, 0), bytes32(uint256(123)), "Read index 0, uint256");
         Assert.equal(bytesUtils.pReadBytes32(testData, 1), bytes32(address(this)), "Read index 1, address");
@@ -68,8 +77,7 @@ contract BytesUtilsTest {
         Assert.equal(bytesUtils.pReadBytes32(testData, 3), bytes32(0x789), "Read index 3, bytes32");
     }
 
-    function testReadNonBytesMemory() public {
-        TestBytesUtils bytesUtils = new TestBytesUtils();
+    function testReadNonBytesMemory() external {
         ThrowProxy throwProxy = new ThrowProxy(address(bytesUtils));
         bytes memory testData = buildData(bytes32(uint256(123)), bytes32(address(this)), keccak256("test"), bytes32(0x789));
 
@@ -82,8 +90,7 @@ contract BytesUtilsTest {
         Assert.isFalse(throwProxy.execute.gas(200000)(), "Read index 0 should fail, it's not inside bytes array");
     }
 
-    function testInvalidLengthBytes() public {
-        TestBytesUtils bytesUtils = new TestBytesUtils();
+    function testInvalidLengthBytes() external {
         ThrowProxy throwProxy = new ThrowProxy(address(bytesUtils));
         bytes memory testData = buildData(bytes32(uint256(123)), bytes32(address(this)), bytes4(keccak256("test")));
 
@@ -94,5 +101,28 @@ contract BytesUtilsTest {
         // Reading index 2 should fail, the word has less than 32 bytes
         BytesUtilsInterface(address(throwProxy)).pReadBytes32(testData, 2);
         Assert.isFalse(throwProxy.execute.gas(200000)(), "Reading index 3 should fail, the word has less than 32 bytes");
+    }
+
+    function testReadOffset() external {
+        bytes memory testData = buildData(bytes32(uint256(123)), bytes32(address(this)), keccak256("test"), bytes32(0x789));
+        Assert.equal(bytesUtils.pRead(testData, 0, 32), bytes32(uint256(123)), "Read index 0, uint256");
+        Assert.equal(bytesUtils.pRead(testData, 32, 32), bytes32(address(this)), "Read index 1, address");
+        Assert.equal(bytesUtils.pRead(testData, 64, 32), keccak256("test"), "Read index 2, bytes32");
+        Assert.equal(bytesUtils.pRead(testData, 96, 32), bytes32(0x789), "Read index 3, bytes32");
+    }
+
+    function testReadOffsetPacked() external {
+        bytes32 test4 = keccak256("test4");
+        bytes32 test5 = keccak256("test4");
+        bytes32 test6 = keccak256("test4");
+        bytes memory data = abi.encodePacked(uint8(12), address(this), test4, test5, test6, uint128(124), true, uint16(5355));
+        Assert.equal(bytesUtils.pRead(data, 0, 1), bytes32(uint8(12)), "Read value 0");
+        Assert.equal(address(bytesUtils.pRead(data, 1, 20)), address(this), "Read value 1");
+        Assert.equal(bytesUtils.pRead(data, 1 + 20, 32), test4, "Read value 2");
+        Assert.equal(bytesUtils.pRead(data, 1 + 20 + 32, 32), test5, "Read value 3");
+        Assert.equal(bytesUtils.pRead(data, 1 + 20 + 32 + 32, 32), test6, "Read value 4");
+        Assert.equal(uint256(bytesUtils.pRead(data, 1 + 20 + 32 + 32 + 32, 16)), uint256(124), "Read value 5");
+        Assert.equal(bytesUtils.pRead(data, 1 + 20 + 32 + 32 + 32 + 16, 1), bytes32(1), "Read value 6");
+        Assert.equal(bytesUtils.pRead(data, 1 + 20 + 32 + 32 + 32 + 16 + 1, 2), bytes32(uint16(5355)), "Read value 7");
     }
 }
