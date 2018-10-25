e6e58f3124e9118560c47fae4deb6ce86535f8cd
Agustin Aguilar <agustin@ripiocredit.network>
2018-10-25 19:19:52 +0200

    Remove override supportsInterface

--- contracts/utils/ERC721Base.sol
+++ contracts/utils/ERC721Base.sol
@@ -455,17 +455,6 @@ contract ERC721Base is ERC165, Ownable {
         emit Transfer(holder, _to, _assetId);
     }
 
-    /**
-     * @dev Returns `true` if the contract implements `interfaceID` and `interfaceID` is not 0xffffffff, `false` otherwise
-     * @param    _interfaceID The interface identifier, as specified in ERC-165
-     */
-    function supportsInterface(bytes4 _interfaceID) external view returns (bool) {
-        if (_interfaceID == 0xffffffff) {
-            return false;
-        }
-        return _interfaceID == 0x01ffc9a7 || _interfaceID == 0x80ac58cd;
-    }
-
     //
     // Utilities
     //
