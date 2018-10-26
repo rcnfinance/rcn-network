c57ee55c1323f83841287d527108d450ae51f18a
Agustin Aguilar <agustin@ripiocredit.network>
2018-10-26 17:58:19 +0200

    Removed migrations stub and add mkdir migrations to test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -4,6 +4,7 @@ set -e
  
 ganache-cli --gasLimit 10000000 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
+mkdir migrations
 rm -rf build
 truffle test
 kill -9 $(lsof -t -i:8545)
