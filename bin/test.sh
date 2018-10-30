461eba1d52fb950fb352d156211207307b18b56e
Joaquin Pablo Gonzalez <jpgonzalezra@gmail.com>
2018-10-30 17:44:03 -0300

    Add solidity-coverage to travis.

--- bin/test.sh
+++ bin/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
- 
+
 set -e
- 
+
 ganache-cli --gasLimit 10000000 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 mkdir migrations
 rm -rf build
-truffle test
+
+node_modules/.bin/solidity-coverage
+cat coverage/lcov.info | node_modules/.bin/coveralls
+
 kill -9 $(lsof -t -i:8545)
