1be448d205f02ad4e7f67fae5b6e14d75e211dab
Agustin Aguilar <agustin@ripiocredit.network>
2018-10-31 23:21:09 +0100

    Real gas limit ganache-cli

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Start testrpc
-node_modules/.bin/ganache-cli --gasLimit 0xfffffffffff > /dev/null &
+node_modules/.bin/ganache-cli --gasLimit 7000000 > /dev/null &
 
 # Run truffle tests
 node_modules/.bin/truffle test "$@"
