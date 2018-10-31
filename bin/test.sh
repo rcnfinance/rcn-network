b29878d42f6891020051c1bf33d5b17ae8d47c04
Joaquin Pablo Gonzalez <jpgonzalezra@gmail.com>
2018-10-30 22:06:42 -0300

    test travis

--- bin/test.sh
+++ bin/test.sh
@@ -7,7 +7,4 @@ sleep 5 # to make sure ganache-cli is up and running before compiling
 mkdir migrations
 rm -rf build
 
-node_modules/.bin/solidity-coverage
-cat coverage/lcov.info | node_modules/.bin/coveralls
-
 kill -9 $(lsof -t -i:8545)
