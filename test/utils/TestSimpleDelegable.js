54799aff59eb0c375f2907aa03d6dc6d3d8e9cee
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-05 17:33:11 +0100

    Fix solidity-coverage run

--- test/utils/TestSimpleDelegable.js
+++ test/utils/TestSimpleDelegable.js
@@ -9,31 +9,31 @@ contract('SimpleDelegable', function(accounts) {
         const delegable = await SimpleDelegable.new();
         await delegable.addDelegate(accounts[2]);
         await delegable.addDelegate(accounts[4]);
-        (await delegable.isDelegate(accounts[2])).should.be.equal(true);
-        (await delegable.isDelegate(accounts[4])).should.be.equal(true);
+        assert.equal(true, await delegable.isDelegate(accounts[2]));
+        assert.equal(true, await delegable.isDelegate(accounts[4]));
     });
     it("Should remove delegates", async function(){
         const delegable = await SimpleDelegable.new();
         await delegable.addDelegate(accounts[2]);
         await delegable.addDelegate(accounts[4]);
         await delegable.removeDelegate(accounts[2]);
-        (await delegable.isDelegate(accounts[2])).should.be.equal(false);
-        (await delegable.isDelegate(accounts[4])).should.be.equal(true);
+        assert.equal(false, await delegable.isDelegate(accounts[2]));
+        assert.equal(true, await delegable.isDelegate(accounts[4]));
     });
     it("Only owner should add delegates", async function() {
         const delegable = await SimpleDelegable.new({ from: accounts[3] });
         await Helper.assertThrow(delegable.addDelegate(accounts[2]));
         await Helper.assertThrow(delegable.addDelegate(accounts[3]));
         await Helper.assertThrow(delegable.addDelegate(accounts[0]));
-        (await delegable.isDelegate(accounts[2])).should.be.equal(false);
-        (await delegable.isDelegate(accounts[3])).should.be.equal(false);
-        (await delegable.isDelegate(accounts[0])).should.be.equal(false);
+        assert.equal(false, await delegable.isDelegate(accounts[2]));
+        assert.equal(false, await delegable.isDelegate(accounts[3]));
+        assert.equal(false, await delegable.isDelegate(accounts[0]));
     });
     it("Only owner should remove delegates", async function() {
         const delegable = await SimpleDelegable.new({ from: accounts[3] });
         await delegable.addDelegate(accounts[2], { from: accounts[3] });
         await Helper.assertThrow(delegable.removeDelegate(accounts[2]));
         await Helper.assertThrow(delegable.removeDelegate(accounts[0]));
-        (await delegable.isDelegate(accounts[2])).should.be.equal(true);
+        assert.equal(true, await delegable.isDelegate(accounts[2]));
     });
 })
