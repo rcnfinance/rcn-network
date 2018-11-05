17b421ec34a3eb6497ae0b085decfd7be4129930
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-05 16:55:23 +0100

    Add simple delegable tests

---
+++ test/SimpleDelegableTest.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.24;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+
+import "../contracts/utils/SimpleDelegable.sol";
+
+contract SimpleDelegableMock is SimpleDelegable {
+    function ping() external onlyDelegate returns (bytes32) {
+        return bytes32(2);
+    }
+}
+
+contract SimpleDelegableTest {
+    function testCallAsDelegate() external {
+        SimpleDelegableMock delegable = new SimpleDelegableMock();
+        delegable.addDelegate(this);
+        Assert.equal(delegable.ping(), bytes32(2), "Call should success");
+    }
+
+    function testFailNonDelegate() external {
+        SimpleDelegableMock delegable = new SimpleDelegableMock();
+        (uint256 success, bytes32 result) = _safeCall(
+            delegable,
+            abi.encodePacked(delegable.ping.selector)
+        );
+        Assert.equal(success, uint256(0), "Call should fail");
+        Assert.equal(result, bytes32(0), "Should return 0");
+    }
+
+    function _safeCall(
+        address _contract,
+        bytes _data
+    ) internal returns (uint256 success, bytes32 result) {
+        assembly {
+            let x := mload(0x40)
+            success := call(
+                            gas,                 // Send almost all gas
+                            _contract,            // To addr
+                            0,                    // Send ETH
+                            add(0x20, _data),     // Input is data past the first 32 bytes
+                            mload(_data),         // Input size is the lenght of data
+                            x,                    // Store the ouput on x
+                            0x20                  // Output is a single bytes32, has 32 bytes
+                        )
+
+            result := mload(x)
+        }
+    }
+}
