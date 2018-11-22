78e88c552f348b281f5994f1fe3c52c2e9514485
Joaquin Pablo Gonzalez <jpgonzalezra@gmail.com>
2018-11-22 12:51:37 -0300

    Ref -> Explicitness Requirements

--- contracts/diaspore/interfaces/Model.sol
+++ contracts/diaspore/interfaces/Model.sol
@@ -126,7 +126,7 @@ contract Model is IERC165 {
 
         @return True if the data can be used to create a new registry
     */
-    function validate(bytes data) external view returns (bool isValid);
+    function validate(bytes memory data) public view returns (bool isValid);
 
     // ///
     // Getters
@@ -267,7 +267,7 @@ contract Model is IERC165 {
 
         @return success True if the registry was created
     */
-    function create(bytes32 id, bytes data) external returns (bool success);
+    function create(bytes32 id, bytes memory data) public returns (bool success);
 
     /**
         If the registry is fully paid on the call and the amount parameter exceeds the required
