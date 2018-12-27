8ed8f5d8fdb806cdc3ba2dd932538becf69cc1b2
rotcivegaf <victorfage@gmail.com>
2018-12-27 16:47:13 -0300

    add functions in interface

--- contracts/diaspore/interfaces/ILoanManager.sol
+++ contracts/diaspore/interfaces/ILoanManager.sol
@@ -3,11 +3,14 @@ pragma solidity ^0.5.0;
 
 interface ILoanManager {
     function debtEngine() external view returns (address);
+    function getBorrower(uint256 _id) external view returns (address);
+    function getCreator(uint256 _id) external view returns (address);
     function getStatus(uint256 _id) external view returns (uint256);
     function getDueTime(uint256 _id) external view returns (uint256);
     function ownerOf(uint256 _id) external view returns (address);
     function getOracle(uint256 _id) external view returns (address);
     function getClosingObligation(uint256 _id) external view returns (uint256);
+    function isApproved(uint256 _id) external view returns (bool);
 
     function cosign(uint256 _id, uint256 _cost) external returns (bool);
     function safeTransferFrom(address _from, address _to, uint256 _assetId) external;
