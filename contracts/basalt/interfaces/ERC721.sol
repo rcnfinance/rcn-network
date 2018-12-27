b1c7e602c01de6fc475eb6c014377b20779c400a
rotcivegaf <victorfage@gmail.com>
2018-12-27 16:42:13 -0300

    update to solidity 0.5

--- contracts/basalt/interfaces/ERC721.sol
+++ contracts/basalt/interfaces/ERC721.sol
@@ -3,20 +3,20 @@ pragma solidity ^0.5.0;
 
 interface ERC721 {
     // ERC20 compatible functions
-    function name() public view returns (string memory _name);
-    function symbol() public view returns (string memory _symbol);
-    function totalSupply() public view returns (uint256 _totalSupply);
-    function balanceOf(address _owner) public view returns (uint _balance);
+    function name() external view returns (string memory _name);
+    function symbol() external view returns (string memory _symbol);
+    function totalSupply() external view returns (uint256 _totalSupply);
+    function balanceOf(address _owner) external view returns (uint _balance);
     // Functions that define ownership
-    function ownerOf(uint256) public view returns (address owner);
-    function approve(address, uint256) public returns (bool);
-    function takeOwnership(uint256) public returns (bool);
-    function transfer(address, uint256) public returns (bool);
-    function setApprovalForAll(address _operator, bool _approved) public returns (bool);
-    function getApproved(uint256 _tokenId) public view returns (address);
-    function isApprovedForAll(address _owner, address _operator) public view returns (bool);
+    function ownerOf(uint256) external view returns (address owner);
+    function approve(address, uint256) external returns (bool);
+    function takeOwnership(uint256) external returns (bool);
+    function transfer(address, uint256) external returns (bool);
+    function setApprovalForAll(address _operator, bool _approved) external returns (bool);
+    function getApproved(uint256 _tokenId) external view returns (address);
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
     // Token metadata
-    function tokenMetadata(uint256 _tokenId) public view returns (string memory info);
+    function tokenMetadata(uint256 _tokenId) external view returns (string memory info);
     // Events
     event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
     event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
