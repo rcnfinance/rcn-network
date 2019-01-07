95c0fb926ad67658b05679f611c57354985898c5
rotcivegaf <victorfage@gmail.com>
2019-01-07 19:01:44 -0300

    fix bundle interface

--- contracts/diaspore/cosigner/pawn/interfaces/IBundle.sol
+++ contracts/diaspore/cosigner/pawn/interfaces/IBundle.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.5.0;
 import "./../../../../interfaces/IERC721Base.sol";
 
 
-interface IBundle {
+contract IBundle is IERC721Base {
     event Created(address _owner, uint256 _id);
     event Deposit(address _sender, uint256 _bundle, IERC721Base _token, uint256 _id);
     event Withdraw(address _retriever, uint256 _bundle, IERC721Base _token, uint256 _id);
 
-    function canDeposit(uint256 _packageId) external view returns (bool);
+    function canDeposit(uint256 _packageId) public view returns (bool);
     function content(uint256 _id) external view returns (address[] memory tokens, uint256[] memory ids);
 
-    function create() external returns (uint256 id);
+    function create() public returns (uint256 id);
     function deposit(uint256 _packageId, IERC721Base _token, uint256 _tokenId) external returns (bool);
     function depositBatch(uint256 _packageId, IERC721Base[] calldata _tokens, uint256[] calldata _ids) external returns (bool);
     function withdraw(uint256 _packageId, IERC721Base _token, uint256 _tokenId, address _to) external returns (bool);
