610bf826fd3d3ca11862ad61479b63d1ccaaa3f2
rotcivegaf <victorfage@gmail.com>
2019-01-17 16:28:22 -0300

    add _to to destroy function and event on Poach interface

--- contracts/diaspore/cosigner/pawn/interfaces/IPoach.sol
+++ contracts/diaspore/cosigner/pawn/interfaces/IPoach.sol
@@ -9,11 +9,11 @@ contract IPoach is IERC721Base {
 
     event Created(uint256 _pairId, address _owner, Token _erc20, uint256 _amount);
     event Deposit(uint256 _pairId, address _sender, uint256 _amount);
-    event Destroy(uint256 _pairId, address _sender, address _to, uint256 _amount);
+    event Destroy(uint256 _pairId, address _sender, address _to, uint256 _balance);
 
     function getPair(uint256 _id) external view returns(Token, uint256);
 
     function create(Token _token, uint256 _amount) external payable returns (uint256 id);
     function deposit(uint256 _id, uint256 _amount) external payable returns (bool);
-    function destroy(uint256 _id) external returns (bool);
+    function destroy(uint256 _id, address payable _to) external returns (bool);
 }
