907dab0ae0cda93a84b4ff4dc2aa35c8cd56ed0a
rotcivegaf <victorfage@gmail.com>
2019-01-30 17:22:02 -0300

    move ownable to basalt folder

---
+++ test/utils/TestOwnableBasalt.js
@@ -0,0 +1,56 @@
+const OwnableBasalt = artifacts.require('../basalt/utils/OwnableBasalt.sol');
+const Helper = require('../Helper.js');
+
+contract('OwnableBasalt', function (accounts) {
+    const owner = accounts[1];
+    const secondOwner = accounts[2];
+    const thirdOwner = accounts[3];
+
+    it('Should change owner on transfer', async function () {
+        const ownable = await OwnableBasalt.new({ from: owner });
+        await ownable.transferTo(secondOwner, { from: owner });
+
+        assert.equal(await ownable.owner(), secondOwner);
+    });
+
+    it('Should revert if try to transfer to 0x0', async function () {
+        const ownable = await OwnableBasalt.new({ from: owner });
+
+        await Helper.tryCatchRevert(
+            () => ownable.transferTo(
+                Helper.address0x,
+                { from: owner }
+            ),
+            '0x0 Is not a valid owner'
+        );
+
+        assert.equal(await ownable.owner(), owner);
+    });
+
+    it('Should revert if another account tries to transfer', async function () {
+        const ownable = await OwnableBasalt.new({ from: owner });
+
+        await Helper.tryCatchRevert(
+            () => ownable.transferTo(
+                secondOwner,
+                { from: secondOwner }
+            ),
+            'The owner should be the sender'
+        );
+
+        await Helper.tryCatchRevert(
+            () => ownable.transferTo(
+                thirdOwner,
+                { from: secondOwner }
+            ),
+            'The owner should be the sender'
+        );
+
+        assert.equal(await ownable.owner(), owner);
+    });
+
+    it('Should be creator with caller as owner', async function () {
+        const ownable = await OwnableBasalt.new({ from: accounts[7] });
+        assert.equal(await ownable.owner(), accounts[7]);
+    });
+});
