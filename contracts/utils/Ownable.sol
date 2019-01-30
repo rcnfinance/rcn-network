c6389c9b47c21a52a506fc9b98a1c0239d797b95
rotcivegaf <victorfage@gmail.com>
2019-01-30 17:26:59 -0300

    upgrade Ownable.sol to respect ERC173

--- contracts/utils/Ownable.sol
+++ contracts/utils/Ownable.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.5.0;
 
+import "./../interfaces/IERC173.sol";
 
-contract Ownable {
+
+contract Ownable is IERC173 {
     address public owner;
+
+    event OwnershipTransferred(address indexed _previousOwner, address indexed _newOwner);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "The owner should be the sender");
@@ -11,16 +15,17 @@ contract Ownable {
 
     constructor() public {
         owner = msg.sender;
+        emit OwnershipTransferred(address(0x0), msg.sender);
     }
 
     /**
         @dev Transfers the ownership of the contract.
 
-        @param _to Address of the new owner
+        @param _newOwner Address of the new owner
     */
-    function transferTo(address _to) public onlyOwner returns (bool) {
-        require(_to != address(0), "0x0 Is not a valid owner");
-        owner = _to;
-        return true;
+    function transferOwnership(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "0x0 Is not a valid owner");
+        emit OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
     }
 }
