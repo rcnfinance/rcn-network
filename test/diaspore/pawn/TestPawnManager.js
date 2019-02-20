59d9c6b1b3cebb9ae7e0f1b1ab421ed9b6b3f43e
rotcivegaf <victorfage@gmail.com>
2019-02-20 18:18:22 -0300

    add requestPawnId and fix tests

--- test/diaspore/pawn/TestPawnManager.js
+++ test/diaspore/pawn/TestPawnManager.js
@@ -50,6 +50,7 @@ contract('TestBundle', function (accounts) {
         debtEngine = await DebtEngine.new(erc20.address);
         loanManager = await LoanManager.new(debtEngine.address);
         model = await TestModel.new();
+        await model.setEngine(debtEngine.address);
 
         bundle = await Bundle.new();
         poach = await Poach.new();
@@ -66,13 +67,11 @@ contract('TestBundle', function (accounts) {
     describe('requestPawn and _createPackage functions', function () {
         it('Should request pawn', async () => {
             const borrower = user;
-            const creator = accounts[1];
+            const creator = accounts[2];
             const salt = bn(web3.utils.randomHex(32));
             const amount = bn('1');
             const expiration = (await Helper.getBlockTime()) + 1000;
             const loanData = await model.encodeData(amount, expiration);
-            await erc20.setBalance(creator, amount);
-            await erc20.approve(pawnManager.address, amount, { from: creator });
 
             const loanId = await loanManager.calcId(
                 amount,
@@ -86,6 +85,9 @@ contract('TestBundle', function (accounts) {
             );
             const signature = await web3.eth.sign(loanId, borrower);
 
+            await erc20.setBalance(creator, '1');
+            await erc20.approve(pawnManager.address, '1', { from: creator });
+
             const erc20s = [ETH, erc20.address];
             const amounts = ['1', '1'];
             const assetId = await generateERC721(erc721, creator);
@@ -93,8 +95,8 @@ contract('TestBundle', function (accounts) {
             const erc721s = [erc721.address];
             const erc721Ids = [assetId];
 
-            const pawnId = (await pawnManager.allTokens()).length;
-            const packageId = (await bundle.allTokens()).length;
+            const pawnId = await pawnManager.pawnsLength();
+            const packageId = await bundle.packagesLength();
 
             const RequestedPawn = await Helper.toEvents(
                 await pawnManager.requestPawn(
@@ -410,6 +412,320 @@ contract('TestBundle', function (accounts) {
                     { from: creator, value: '4' }
                 ),
                 ''
+            );
+        });
+    });
+
+    describe('requestPawnId function', function () {
+        it('Should request pawn with a loan', async () => {
+            const borrower = user;
+            const creator = accounts[2];
+            const salt = bn(web3.utils.randomHex(32));
+            const amount = bn('1');
+            const expiration = (await Helper.getBlockTime()) + 1000;
+            const loanData = await model.encodeData(amount, expiration);
+
+            const loanId = await loanManager.calcId(
+                amount,
+                borrower,
+                creator,
+                model.address,
+                Helper.address0x,
+                salt,
+                expiration,
+                loanData
+            );
+
+            await loanManager.requestLoan(
+                amount,            // Amount
+                model.address,     // Model
+                Helper.address0x,  // Oracle
+                borrower,          // Borrower
+                salt,              // salt
+                expiration,        // Expiration
+                loanData,          // Loan data
+                { from: creator } // Creator
+            );
+
+            await loanManager.approveRequest(loanId, { from: borrower });
+
+            await erc20.setBalance(creator, '1');
+            await erc20.approve(pawnManager.address, '1', { from: creator });
+
+            const erc20s = [ETH, erc20.address];
+            const amounts = ['1', '1'];
+            const assetId = await generateERC721(erc721, creator);
+
+            const erc721s = [erc721.address];
+            const erc721Ids = [assetId];
+
+            const pawnId = await pawnManager.pawnsLength();
+            const packageId = await bundle.packagesLength();
+
+            const RequestedPawn = await Helper.toEvents(
+                await pawnManager.requestPawnId(
+                    loanManager.address,
+                    loanId,
+                    erc20s, // ERC20 Tokens addresses
+                    amounts, // ERC20 amounts
+                    erc721s, // ERC721 Tokens addresses
+                    erc721Ids, // ERC721 ids
+                    { from: creator, value: '1' }
+                ),
+                'RequestedPawn'
+            );
+            expect(RequestedPawn._pawnId).to.eq.BN(pawnId);
+            expect(RequestedPawn._loanId).to.eq.BN(loanId);
+            expect(RequestedPawn._creator).to.eq.BN(creator);
+            expect(RequestedPawn._borrower).to.eq.BN(borrower);
+            expect(RequestedPawn._loanManager).to.eq.BN(loanManager.address);
+            expect(RequestedPawn._packageId).to.eq.BN(packageId);
+
+            const pawn = await pawnManager.pawns(pawnId);
+            assert.equal(pawn.owner, borrower);
+            assert.equal(pawn.loanManager, loanManager.address);
+            assert.equal(pawn.loanId, loanId);
+            expect(pawn.packageId).to.eq.BN(packageId);
+            expect(pawn.status).to.eq.BN(PENDING);
+
+            const request = await loanManager.requests(loanId);
+            assert.equal(request.open, true);
+            assert.equal(request.approved, true);
+            expect(request.position).to.eq.BN((await loanManager.getDirectoryLength()).sub(bn('1')));
+            expect(request.expiration).to.eq.BN(expiration);
+            assert.equal(await loanManager.getCurrency(loanId), 0x0);
+            expect(request.amount).to.eq.BN(amount);
+            assert.equal(request.cosigner, Helper.address0x);
+            assert.equal(request.model, model.address);
+            assert.equal(request.creator, creator);
+            assert.equal(request.oracle, Helper.address0x);
+            assert.equal(request.borrower, borrower);
+            expect(request.salt).to.eq.BN(salt);
+            assert.equal(request.loanData, loanData);
+            expect(await loanManager.getStatus(loanId)).to.eq.BN('0');
+            expect(await loanManager.getDueTime(loanId)).to.eq.BN('0');
+
+            const content = await bundle.content(packageId);
+
+            assert.equal(content[ERC721S][0], poach.address);
+            const pairETHId = (await poach.poachesLength()).sub(bn('2'));
+            expect(content[ERC721IDS][0]).to.eq.BN(pairETHId);
+            const pairETH = await poach.poaches(pairETHId);
+            assert.equal(pairETH.token, ETH);
+            expect(pairETH.balance).to.eq.BN(amounts[0]);
+
+            assert.equal(content[ERC721S][1], poach.address);
+            const pairERC20Id = dec(await poach.poachesLength());
+            expect(content[ERC721IDS][1]).to.eq.BN(pairERC20Id);
+            const pairERC20 = await poach.poaches(pairERC20Id);
+            assert.equal(pairERC20.token, erc20.address);
+            expect(pairERC20.balance).to.eq.BN(amounts[1]);
+
+            assert.equal(content[ERC721S][2], erc721.address);
+            expect(content[ERC721IDS][2]).to.eq.BN(assetId);
+
+            assert.equal(await poach.ownerOf(pairETHId), bundle.address);
+            assert.equal(await poach.ownerOf(pairERC20Id), bundle.address);
+            assert.equal(await bundle.ownerOf(packageId), pawnManager.address);
+
+            expect(await pawnManager.loanToLiability(loanManager.address, loanId)).to.eq.BN(pawnId);
+        });
+
+        it('Try request a pawn with a lended loan', async () => {
+            const borrower = user;
+            const creator = accounts[1];
+            const salt = bn(web3.utils.randomHex(32));
+            const amount = bn('1');
+            const expiration = (await Helper.getBlockTime()) + 1000;
+            const loanData = await model.encodeData(amount, expiration);
+
+            const loanId = await loanManager.calcId(
+                amount,
+                borrower,
+                borrower,
+                model.address,
+                Helper.address0x,
+                salt,
+                expiration,
+                loanData
+            );
+
+            await loanManager.requestLoan(
+                amount,            // Amount
+                model.address,     // Model
+                Helper.address0x,  // Oracle
+                borrower,          // Borrower
+                salt,              // salt
+                expiration,        // Expiration
+                loanData,          // Loan data
+                { from: borrower } // Creator
+            );
+
+            await erc20.setBalance(creator, '1');
+            await erc20.approve(loanManager.address, '1', { from: creator });
+
+            await loanManager.lend(
+                loanId,
+                [],
+                Helper.address0x,
+                '0',
+                [],
+                { from: creator }
+            );
+
+            await Helper.tryCatchRevert(
+                () => pawnManager.requestPawnId(
+                    loanManager.address,
+                    loanId,
+                    [], // ERC20 Tokens addresses
+                    [], // ERC20 amounts
+                    [], // ERC721 Tokens addresses
+                    [], // ERC721 ids
+                    { from: creator }
+                ),
+                'The loan request should be open'
+            );
+        });
+
+        it('Try request a pawn with a unapproved loan', async () => {
+            const borrower = user;
+            const creator = accounts[2];
+            const salt = bn(web3.utils.randomHex(32));
+            const amount = bn('1');
+            const expiration = (await Helper.getBlockTime()) + 1000;
+            const loanData = await model.encodeData(amount, expiration);
+
+            const loanId = await loanManager.calcId(
+                amount,
+                borrower,
+                borrower,
+                model.address,
+                Helper.address0x,
+                salt,
+                expiration,
+                loanData
+            );
+
+            await loanManager.requestLoan(
+                amount,            // Amount
+                model.address,     // Model
+                Helper.address0x,  // Oracle
+                borrower,          // Borrower
+                salt,              // salt
+                expiration,        // Expiration
+                loanData,          // Loan data
+                { from: creator } // Creator
+            );
+
+            await Helper.tryCatchRevert(
+                () => pawnManager.requestPawnId(
+                    loanManager.address,
+                    loanId,
+                    [], // ERC20 Tokens addresses
+                    [], // ERC20 amounts
+                    [], // ERC721 Tokens addresses
+                    [], // ERC721 ids
+                    { from: creator }
+                ),
+                'The loan its not approve'
+            );
+        });
+
+        it('Try request a pawn with a loan without be the borrower or the creator', async () => {
+            const borrower = user;
+            const creator = user;
+            const salt = bn(web3.utils.randomHex(32));
+            const amount = bn('1');
+            const expiration = (await Helper.getBlockTime()) + 1000;
+            const loanData = await model.encodeData(amount, expiration);
+
+            const loanId = await loanManager.calcId(
+                amount,
+                borrower,
+                borrower,
+                model.address,
+                Helper.address0x,
+                salt,
+                expiration,
+                loanData
+            );
+
+            await loanManager.requestLoan(
+                amount,            // Amount
+                model.address,     // Model
+                Helper.address0x,  // Oracle
+                borrower,          // Borrower
+                salt,              // salt
+                expiration,        // Expiration
+                loanData,          // Loan data
+                { from: creator } // Creator
+            );
+
+            await Helper.tryCatchRevert(
+                () => pawnManager.requestPawnId(
+                    loanManager.address,
+                    loanId,
+                    [], // ERC20 Tokens addresses
+                    [], // ERC20 amounts
+                    [], // ERC721 Tokens addresses
+                    [], // ERC721 ids
+                    { from: accounts[9] }
+                ),
+                'The sender should be the borrower or the creator'
+            );
+        });
+
+        it('Try request a pawn with a loan and the pawn was requested', async () => {
+            const borrower = user;
+            const creator = user;
+            const salt = bn(web3.utils.randomHex(32));
+            const amount = bn('1');
+            const expiration = (await Helper.getBlockTime()) + 1000;
+            const loanData = await model.encodeData(amount, expiration);
+
+            const loanId = await loanManager.calcId(
+                amount,
+                borrower,
+                borrower,
+                model.address,
+                Helper.address0x,
+                salt,
+                expiration,
+                loanData
+            );
+
+            await loanManager.requestLoan(
+                amount,            // Amount
+                model.address,     // Model
+                Helper.address0x,  // Oracle
+                borrower,          // Borrower
+                salt,              // salt
+                expiration,        // Expiration
+                loanData,          // Loan data
+                { from: creator } // Creator
+            );
+
+            await pawnManager.requestPawnId(
+                loanManager.address,
+                loanId,
+                [], // ERC20 Tokens addresses
+                [], // ERC20 amounts
+                [], // ERC721 Tokens addresses
+                [], // ERC721 ids
+                { from: borrower }
+            );
+
+            await Helper.tryCatchRevert(
+                () => pawnManager.requestPawnId(
+                    loanManager.address,
+                    loanId,
+                    [], // ERC20 Tokens addresses
+                    [], // ERC20 amounts
+                    [], // ERC721 Tokens addresses
+                    [], // ERC721 ids
+                    { from: creator }
+                ),
+                'The liability its taken'
             );
         });
     });
