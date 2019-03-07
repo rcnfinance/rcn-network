2c73fc2533c3a69f06af314b0912dc5467015036
rotcivegaf <victorfage@gmail.com>
2019-03-07 18:52:29 -0300

    fix solium

--- contracts/diaspore/cosigner/pawn/interfaces/IMechanism.sol
+++ contracts/diaspore/cosigner/pawn/interfaces/IMechanism.sol
@@ -2,20 +2,8 @@ pragma solidity ^0.5.0;
 
 
 contract IMechanism {
-    function addBalance(
-        uint256 _pawnId,
-        uint256 _packageId,
-        uint256 _order,
-        uint256 _amount
-    ) external;
-
-    function takeBalance(
-        uint256 _pawnId,
-        uint256 _packageId,
-        uint256 _order,
-        uint256 _amount,
-        address _to
-    ) external;
+    function addBalance(uint256 _pawnId, uint256 _packageId, uint256 _order, uint256 _amount) external;
+    function takeBalance(uint256 _pawnId, uint256 _packageId, uint256 _order, uint256 _amount, address _to) external;
 
     function buyBalance() external;
     function sellBalance() external;
