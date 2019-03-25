897f7d152e43616033028bd1fd66a07ef610ce0b
Agustin Aguilar <Agusxrun@gmail.com>
2019-03-25 21:52:47 +0100

    Add Diaspore utils libs

--- contracts/core/diaspore/utils/DiasporeUtils.sol
+++ contracts/core/diaspore/utils/DiasporeUtils.sol
@@ -1,10 +1,18 @@
 pragma solidity ^0.5.6;
 
+import "../../../utils/SafeERC20.sol";
+import "../../../utils/SafeMath.sol";
+import "../../../interfaces/IERC20.sol";
 import "../interfaces/RateOracle.sol";
+import "../interfaces/Model.sol";
 import "../LoanManager.sol";
+import "../DebtEngine.sol";
 
 
 library DiasporeUtils {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
     function amountToToken(
         LoanManager _manager,
         bytes32 _id,
@@ -20,5 +28,49 @@ library DiasporeUtils {
             (uint256 tokens, uint256 equivalent) = oracle.readSample(_oracleData);
             return (tokens * amount) / equivalent;
         }
+    }
+
+    function readOracle(
+        LoanManager _manager,
+        bytes32 _id,
+        bytes memory _oracleData
+    ) internal returns (uint256, uint256) {
+        RateOracle oracle = RateOracle(_manager.getOracle(_id));
+        if (address(oracle) == address(0)) {
+            return (0, 0);
+        } else {
+            return oracle.readSample(_oracleData);
+        }
+    }
+
+    function safePayToken(
+        LoanManager _manager,
+        bytes32 _id,
+        uint256 _amount,
+        address _sender,
+        bytes memory _oracleData
+    ) internal returns (uint256 paid, uint256 tokens) {
+        IERC20 token = IERC20(_manager.token());
+        DebtEngine engine = DebtEngine(_manager.debtEngine());
+        require(token.safeApprove(address(engine), _amount), "Error approve debt engine");
+
+        uint256 prevBalance = token.balanceOf(address(this));
+
+        (paid, tokens) = engine.payToken(
+            _id,
+            _amount,
+            _sender,
+            _oracleData
+        );
+
+        require(token.clearApprove(address(engine)), "Error clear approve");
+        require(prevBalance.sub(token.balanceOf(address(this))) <= tokens, "Debt engine pulled too many tokens");
+    }
+
+    function getModel(
+        LoanManager _manager,
+        bytes32 _id
+    ) internal returns (Model model) {
+        (,,model,,) = _manager.debtEngine().debts(_id);
     }
 }
