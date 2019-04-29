cb045329e58aa4b40945f3ee3de6d71f45d64895
rotcivegaf <victorfage@gmail.com>
2019-04-29 12:04:49 -0300

    add abs, div, divceil and muldivceil tests

--- test/utils/SafeSignedMathTest.sol
+++ test/utils/SafeSignedMathTest.sol
@@ -9,6 +9,12 @@ import "../../contracts/utils/SafeSignedMath.sol";
 contract SafeSignedMathTest {
     using SafeSignedMath for int256;
 
+    function testShouldAbs() external {
+        Assert.equal(int256(1000).abs(), 1000, "");
+        Assert.equal(int256(-1234).abs(), 1234, "");
+        Assert.equal(int256(0).abs(), 0, "");
+    }
+
     function testShouldAdd() external {
         Assert.equal(int256(1000).add(234), 1234, "");
         Assert.equal(int256(1234).add(-234), 1000, "");
@@ -36,6 +42,12 @@ contract SafeSignedMathTest {
         Assert.equal(int256(-10).mul(-20), 200, "");
     }
 
+    function testShouldDiv() external {
+        Assert.equal(int256(0).div(1234), 0, "");
+        Assert.equal(int256(10).div(3), 3, "");
+        Assert.equal(int256(10).div(10), 1, "");
+    }
+
     function testShouldMulDiv() external {
         Assert.equal(int256(0).muldiv(20, 3), 0, "");
         Assert.equal(int256(20).muldiv(0, 3), 0, "");
@@ -46,5 +58,23 @@ contract SafeSignedMathTest {
         Assert.equal(int256(30).muldiv(30, 31), 29, "");
         Assert.equal(int256(30).muldiv(32, 31), 30, "");
         Assert.equal(int256(32).muldiv(32, 31), 33, "");
+    }
+
+    function testShouldDivCeil() external {
+        Assert.equal(int256(0).divceil(1234), 0, "");
+        Assert.equal(int256(10).divceil(3), 4, "");
+        Assert.equal(int256(10).divceil(10), 1, "");
+    }
+
+    function testShouldMulDivCeil() external {
+        Assert.equal(int256(0).muldivceil(20, 3), 0, "");
+        Assert.equal(int256(20).muldivceil(0, 3), 0, "");
+        Assert.equal(int256(34).muldivceil(13, 3), 148, "");
+        Assert.equal(int256(10).muldivceil(20, 5), 40, "");
+        Assert.equal(int256(10).muldivceil(10, 3), 34, "");
+        Assert.equal(int256(20).muldivceil(10, 3), 67, "");
+        Assert.equal(int256(30).muldivceil(30, 31), 30, "");
+        Assert.equal(int256(30).muldivceil(32, 31), 31, "");
+        Assert.equal(int256(32).muldivceil(32, 31), 34, "");
     }
 }
