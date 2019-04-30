64244bf442e35cd1fad25e8ba0c07d18e369c030
rotcivegaf <victorfage@gmail.com>
2019-04-30 10:41:45 -0300

    add tests for safeSignedMath

---
+++ contracts/test_utils/SafeSignedMathMock.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.5.6;
+
+import "../utils/SafeSignedMath.sol";
+
+
+contract SafeSignedMathMock {
+    using SafeSignedMath for int256;
+
+    function abs(int256 x) external returns (int256) {
+        return x.abs();
+    }
+
+    function add(int256 x, int256 y) external returns (int256) {
+        return x.add(y);
+    }
+
+    function sub(int256 x, int256 y) external returns (int256) {
+        return x.sub(y);
+    }
+
+    function mul(int256 x, int256 y) external returns (int256) {
+        return x.mul(y);
+    }
+
+    function div(int256 x, int256 y) external returns (int256) {
+        return x.div(y);
+    }
+
+    function muldiv(int256 x, int256 y, int256 z) external returns (int256) {
+        return x.muldiv(y, z);
+    }
+
+    function divceil(int256 x, int256 y) external returns (int256) {
+        return x.divceil(y);
+    }
+
+    function muldivceil(int256 x, int256 y, int256 z) external returns (int256) {
+        return x.muldivceil(y, z);
+    }
+}
