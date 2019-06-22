f8f081f58a4322aff5c7805e481ab5e42f139e9c
rotcivegaf <victorfage@gmail.com>
2019-06-22 00:24:37 -0300

    up gas limit on contract deploy

--- truffle.js
+++ truffle.js
@@ -11,6 +11,7 @@ module.exports = {
             host: 'localhost',
             port: 8545,
             network_id: '*', // eslint-disable-line camelcase
+            gas: 8000000,
         },
     },
     compilers: {
