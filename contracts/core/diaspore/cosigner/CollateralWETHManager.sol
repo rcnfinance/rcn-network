abd39f529fe8498cba0d27cfbbd08e6a64bcfeed
rotcivegaf <victorfage@gmail.com>
2019-09-06 12:15:34 -0300

    add doc

--- contracts/core/diaspore/cosigner/CollateralWETHManager.sol
+++ contracts/core/diaspore/cosigner/CollateralWETHManager.sol
@@ -19,6 +19,9 @@ contract CollateralWETHManager is Ownable {
     IWETH9 public weth;
     Collateral public collateral;
 
+    /**
+        @dev Check if the sender is the owner of the collateral
+    */
     modifier isTheOwner(uint256 _entryId) {
         require(collateral.ownerOf(_entryId) == msg.sender, "The sender is not current owner");
         _;
@@ -34,6 +37,11 @@ contract CollateralWETHManager is Ownable {
         emit SetCollateral(_collateral);
     }
 
+    /**
+        @dev Set WETH contract
+
+        @param _weth New WETH
+    */
     function setWeth(IWETH9 _weth) external onlyOwner {
         require(_weth != IWETH9(0), "Error loading WETH");
 
@@ -41,6 +49,11 @@ contract CollateralWETHManager is Ownable {
         emit SetWeth(_weth);
     }
 
+    /**
+        @dev Set collateral contract
+
+        @param _collateral New collateral
+    */
     function setCollateral(Collateral _collateral) external onlyOwner {
         require(_collateral != Collateral(0), "Error loading Collateral");
 
@@ -48,6 +61,19 @@ contract CollateralWETHManager is Ownable {
         emit SetCollateral(_collateral);
     }
 
+    /**
+        @dev Convert the msg.value to WETH, approve collateral to use the WETH
+            and create an entry
+
+        @param _debtId Id of the debt
+        @param _oracle The oracle to get the rate between loanManagerToken and entry token
+
+        @param _liquidationRatio Ratio, when collateral ratio is lower enables the execution of the margin call
+        @param _balanceRatio Ratio, expected collateral ratio after margin call execution
+
+        @param _burnFee Ratio, The burn fee of execute a margin call or pay expired debt, this is sent to the address 0
+        @param _rewardFee Ratio, The reward fee of execute a margin call or pay expired debt, this is sent to the sender of the transactiond
+    */
     function create(
         bytes32 _debtId,
         RateOracle _oracle,
@@ -74,6 +100,12 @@ contract CollateralWETHManager is Ownable {
         emit Created(entryId, msg.sender, msg.value);
     }
 
+    /**
+        @dev Convert the msg.value to WETH, approve collateral to use the WETH
+            and deposit to an entry
+
+        @param _entryId The index of the entry
+    */
     function deposit(
         uint256 _entryId
     ) external payable {
@@ -84,11 +116,23 @@ contract CollateralWETHManager is Ownable {
         emit Deposited(_entryId, msg.sender, msg.value);
     }
 
+    /**
+        @dev Convert the ETH to WETH and approve collateral to use the WETH
+    */
     function depositApprove() internal {
         weth.deposit.value(msg.value)();
         weth.approve(address(collateral), msg.value);
     }
 
+    /**
+        @dev Withdraw an amount of an entry, withdraw the WETH and transfer the ETH
+
+        @param _entryId The index of the entry
+        @param _to The beneficiary of the ETH
+        @param _amount The amount of WETH to be withdraw in ETH
+        @param _oracleData Data of oracle to change the currency of debt
+            to Token of debt engine
+    */
     function withdraw(
         uint256 _entryId,
         address payable _to,
@@ -107,6 +151,12 @@ contract CollateralWETHManager is Ownable {
         emit Withdrawed(_entryId, _to, _amount);
     }
 
+    /**
+        @dev Redeem an entry, withdraw the WETH and transfer the ETH
+
+        @param _entryId The index of the entry
+        @param _to The beneficiary of the ETH
+    */
     function redeem(
         uint256 _entryId,
         address payable _to
@@ -118,6 +168,12 @@ contract CollateralWETHManager is Ownable {
         emit Redeemed(_entryId, _to, amount);
     }
 
+    /**
+        @dev Convert the WETH to ETH and transfer the ETH
+
+        @param _to The beneficiary of the ETH
+        @param _amount The amount of ETH to be transfer
+    */
     function withdrawTransfer(
         address payable _to,
         uint256 _amount
@@ -126,5 +182,8 @@ contract CollateralWETHManager is Ownable {
         _to.transfer(_amount);
     }
 
+    /**
+        @dev Use to receive ETH
+    */
     function () external payable { }
 }
