6032d5fa32abba73576d2d4109f5d64d85958cd5
rotcivegaf <victorfage@gmail.com>
2019-10-16 19:59:02 -0300

    add tests

--- test/diaspore/TestCollateral.js
+++ test/diaspore/TestCollateral.js
@@ -394,7 +394,7 @@ contract('Test Collateral cosigner Diaspore', function (accounts) {
         // Collateral deploy
         collateral = await Collateral.new(loanManager.address, { from: owner });
         await collateral.setConverter(converter.address, { from: owner });
-        await collateral.setMaxSpreadRatio(auxToken.address, 1000, { from: owner });
+        await collateral.setMaxSpreadRatio(auxToken.address, bn(9000), { from: owner });
     });
 
     it('Set new url', async function () {
@@ -1869,70 +1869,111 @@ contract('Test Collateral cosigner Diaspore', function (accounts) {
         });
     });
     describe('Front-running in _convertPay function', function () {
-        // With change oracle rate(RCNequivalent)
-        // Try do front-running with high rate
-        const oracleRateTitle = 'Change oracle rate';
-        it('T0: ' + oracleRateTitle, frontRunningTest(0, WEI.mul(bn(2))));
-        it('T1: ' + oracleRateTitle, frontRunningTest(1000, WEI.mul(bn(2))));
-        it('T2: ' + oracleRateTitle, frontRunningTest(4999, WEI.mul(bn(2))));
-        // Test with low rate
-        it('T3: ' + oracleRateTitle, frontRunningTest(0, WEI.div(bn(2)), false));
-        it('T4: ' + oracleRateTitle, frontRunningTest(1000, WEI.div(bn(2)), false));
-        it('T5: ' + oracleRateTitle, frontRunningTest(4999, WEI.div(bn(2)), false));
-        // With change converter rate(setRate)
-        // Try do front-running with high rate
-        const converterRateTitle = 'Change converter rate';
-        it('T0: ' + converterRateTitle, frontRunningTest(0, WEI.mul(bn(2))), true, false);
-        it('T1: ' + converterRateTitle, frontRunningTest(1000, WEI.mul(bn(2))), true, false);
-        it('T2: ' + converterRateTitle, frontRunningTest(4999, WEI.mul(bn(2))), true, false);
-        // Test with low rate
-        it('T3: ' + converterRateTitle, frontRunningTest(0, WEI.div(bn(2)), false), false);
-        it('T4: ' + converterRateTitle, frontRunningTest(1000, WEI.div(bn(2)), false), false);
-        it('T5: ' + converterRateTitle, frontRunningTest(4999, WEI.div(bn(2)), false), false);
+        it('With 0 spread', frontRunningTest(
+            bn(0), // maxSpreadRatio
+            WEI,   // rate
+            false  // revert
+        ));
+        it('With 0 spread, low rate', frontRunningTest(
+            bn(0),          // maxSpreadRatio
+            WEI.div(bn(2)), // rate
+            false           // revert
+        ));
+        it('With 0 spread, high rate', frontRunningTest(
+            bn(0),          // maxSpreadRatio
+            WEI.mul(bn(2)), // rate
+            false           // revert
+        ));
+        it('With 100% spread', frontRunningTest(
+            BASE, // maxSpreadRatio
+            WEI,  // rate
+            false // revert
+        ));
+        it('With 100% spread, high rate', frontRunningTest(
+            BASE,           // maxSpreadRatio
+            WEI.mul(bn(2)), // rate
+            false           // revert
+        ));
+        it('With 10% spread', frontRunningTest(
+            bn(9000), // maxSpreadRatio
+            WEI,      // rate
+            false     // revert
+        ));
+        it('With 10% spread, high rate', frontRunningTest(
+            bn(9000),       // maxSpreadRatio
+            WEI.mul(bn(2)), // rate
+            false           // revert
+        ));
+        it('With -10% spread, low rate', frontRunningTest(
+            bn(11000),                   // maxSpreadRatio
+            WEI.mul(bn(11)).div(bn(10)), // rate
+            false                        // revert
+        ));
+
+        // Reverts, front running
+        it('With 100% spread, low rate', frontRunningTest(
+            BASE,               // maxSpreadRatio
+            WEI.div(bn(2)) // rate
+        ));
+        it('With 9.99% spread, low rate', frontRunningTest(
+            bn(9001),                   // maxSpreadRatio
+            WEI.mul(bn(9)).div(bn(10)), // rate
+        ));
+        it('With -10% spread, high rate', frontRunningTest(
+            bn(11000),                    // maxSpreadRatio
+            WEI.mul(bn(105)).div(bn(100)) // rate
+        ));
 
         function frontRunningTest (
             maxSpreadRatio,
             rate,
-            revert = true,
-            oracleRate = true
+            revert = true
         ) {
             return async () => {
-                const entry = await new EntryBuilder()
-                    .with('entryAmount', bn(1000000000))
-                    .with('loanAmount', bn(10000))
-                    .with('burnFee', bn(0))
-                    .with('rewardFee', bn(0))
-                    .build();
-
-                await lend(entry);
-
-                const prevMaxSpreadRatio = await collateral.tokenToMaxSpreadRatio(auxToken.address);
-                await collateral.setMaxSpreadRatio(auxToken.address, bn(10000).sub(bn(maxSpreadRatio)), { from: owner });
-
-                if (oracleRate) {
-                    await converter.setRate(rcn.address, entry.collateralToken.address, rate);
-                } else {
-                    await oracle.setEquivalent(rate);
-                }
-
-                await rcn.setBalance(converter.address, bn(2).pow(bn('40')));
-
-                const payOffDebt = () => collateral.payOffDebt(
-                    entry.id,
-                    [],
-                    { from: creator }
-                );
-                if (revert) {
-                    await Helper.tryCatchRevert(
-                        payOffDebt,
-                        'converter return below minimun required'
-                    );
-                } else {
-                    await payOffDebt();
-                }
-
-                await collateral.setMaxSpreadRatio(auxToken.address, prevMaxSpreadRatio, { from: owner });
+                // With change oracle rate(RCNequivalent)
+                await _frontRunningTest(maxSpreadRatio, rate, revert);
+                // With change converter rate(setRate)
+                await _frontRunningTest(maxSpreadRatio, rate, revert, true);
             };
+        }
+
+        async function _frontRunningTest (maxSpreadRatio, rate, revert, oracleRate = false) {
+            const entry = await new EntryBuilder()
+                .with('entryAmount', bn(1000000000))
+                .with('loanAmount', bn(10000))
+                .with('burnFee', bn(0))
+                .with('rewardFee', bn(0))
+                .build();
+
+            await lend(entry);
+
+            const prevMaxSpreadRatio = await collateral.tokenToMaxSpreadRatio(auxToken.address);
+            await collateral.setMaxSpreadRatio(auxToken.address, bn(maxSpreadRatio), { from: owner });
+
+            if (oracleRate) {
+                const invRate = WEI.mul(WEI).div(rate);
+                await converter.setRate(rcn.address, entry.collateralToken.address, invRate);
+            } else {
+                await oracle.setEquivalent(rate);
+            }
+
+            await rcn.setBalance(converter.address, bn(2).pow(bn('40')));
+
+            const payOffDebt = () => collateral.payOffDebt(
+                entry.id,
+                [],
+                { from: creator }
+            );
+            if (revert) {
+                await Helper.tryCatchRevert(
+                    payOffDebt,
+                    'converter return below minimun required'
+                );
+            } else {
+                await payOffDebt();
+            }
+
+            await collateral.setMaxSpreadRatio(auxToken.address, prevMaxSpreadRatio, { from: owner });
         }
     });
     describe('Functional test', function () {
