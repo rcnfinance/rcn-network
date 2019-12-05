7d3cea6acb637c058052b09e397b45fdf191f2d4
rotciv <victorfage@gmail.com>
2019-12-05 12:24:18 -0300

    move bn and expect to helper

--- test/Helper.js
+++ test/Helper.js
@@ -1,5 +1,5 @@
 const BN = web3.utils.BN;
-const expect = require('chai')
+module.exports.expect = require('chai')
     .use(require('bn-chai')(BN))
     .expect;
 
@@ -10,6 +10,10 @@ module.exports.STATUS_REQUEST = '0';
 module.exports.STATUS_ONGOING = '1';
 module.exports.STATUS_PAID = '2';
 module.exports.STATUS_ERROR = '4';
+
+module.exports.bn = (number) => {
+    return web3.utils.toBN(number);
+}
 
 module.exports.arrayToBytesOfBytes32 = (array) => {
     let bytes = '0x';
