9c071595608cefb8095791209d4e4d68a7be2afb
rotciv <victorfage@gmail.com>
2019-12-13 15:38:30 -0300

    add BorrowCollateral event

--- contracts/core/diaspore/cosigner/Collateral.sol
+++ contracts/core/diaspore/cosigner/Collateral.sol
@@ -74,6 +74,11 @@ contract Collateral is ReentrancyGuard, Ownable, Cosigner, ERC721Base, Collatera
         address _to
     );
 
+    event BorrowCollateral(
+        CollateralHandler _handler,
+        uint256 _newAmount
+    );
+
     event SetUrl(
         string _url
     );
@@ -319,6 +324,8 @@ contract Collateral is ReentrancyGuard, Ownable, Cosigner, ERC721Base, Collatera
             bytes32 afRatio = entry.ratio(_debtInTokens(debtId, _oracleData));
             require(afRatio.gt(ogRatio), "collateral: ratio should increase");
         }
+
+        emit BorrowCollateral(_handler, surplus);
     }
 
     function auctionClosed(
