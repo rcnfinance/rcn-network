155409333354b5a60af75b71ff1ef8378c006714
rotciv <victorfage@gmail.com>
2019-12-17 14:47:34 -0300

    add mock code

--- contracts/test_utils/diaspore/collateral/TestCollateralAuction.sol
+++ contracts/test_utils/diaspore/collateral/TestCollateralAuction.sol
@@ -1,9 +1,26 @@
 pragma solidity ^0.5.11;
 
 import "../../../interfaces/IERC20.sol";
+import "../../../core/diaspore/cosigner/Collateral.sol";
 
 
 contract TestCollateralAuction {
+    LoanManager public loanManager;
+    IERC20 public loanManagerToken;
+    Collateral public collateral;
+
+    mapping(uint256 => IERC20) public entryToToken;
+    uint256 auctionId;
+
+    constructor(LoanManager _loanManager) public {
+        loanManager = _loanManager;
+        loanManagerToken = loanManager.token();
+    }
+
+    function setCollateral(Collateral _collateral) external {
+        collateral = _collateral;
+    }
+
     function create(
         IERC20 _fromToken,
         uint256 _start,
@@ -11,6 +28,25 @@ contract TestCollateralAuction {
         uint256 _limit,
         uint256 _amount
     ) external returns (uint256 id) {
-        return 1;
+        id = auctionId;
+        entryToToken[id] = _fromToken;
+        auctionId++;
+    }
+
+    function toAuctionClosed(
+        uint256 _id,
+        uint256 _leftover,
+        uint256 _received,
+        bytes calldata _data
+    ) external {
+        entryToToken[_id].transfer(address(collateral), _leftover);
+        loanManagerToken.transfer(address(collateral), _received);
+
+        collateral.auctionClosed(
+            _id,
+            _leftover,
+            _received,
+            _data
+        );
     }
 }
