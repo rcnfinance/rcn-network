48bf7d080fb07aa59db31420193e07ada22fd39f
Agustin <Agusxrun@gmail.com>
2020-01-29 21:55:30 +0100

    Comments for the collateral dutch auction

--- contracts/core/diaspore/cosigner/CollateralAuction.sol
+++ contracts/core/diaspore/cosigner/CollateralAuction.sol
@@ -11,6 +11,16 @@ import "../../../commons/ReentrancyGuard.sol";
 import "./interfaces/CollateralAuctionCallback.sol";
 
 
+/**
+    @title ERC-20 Dutch auction
+    @author Agustin Aguilar <agustin@ripiocredit.network>
+    @notice Auctions tokens in exchange for `baseToken` using a Dutch auction scheme,
+        the owner of the contract is the sole beneficiary of all the auctions.
+        Auctions follow two linear functions to determine the exchange rate that
+        are determined by the provided `reference` rate.
+    @dev If the auction token matches the requested `baseToken`,
+        the auction has a fixed rate of 1:1
+*/
 contract CollateralAuction is ReentrancyGuard, Ownable {
     using IsContract for address payable;
     using SafeERC20 for IERC20;
@@ -50,13 +60,39 @@ contract CollateralAuction is ReentrancyGuard, Ownable {
 
     constructor(IERC20 _baseToken) public {
         baseToken = _baseToken;
+        // Auction IDs start at 1
         auctions.length++;
     }
 
+    /**
+        @notice Returns the size of the auctions array
+
+        @dev The auction with ID 0 is invalid, thus the value
+            returned by this method is the total number of auctions + 1
+
+        @return The size of the auctions array
+    */
     function getAuctionsLength() external view returns (uint256) {
         return auctions.length;
     }
 
+    /**
+        @notice Creates a new auction that starts immediately, any address
+            can start an auction, but the beneficiary of all auctions is the
+            owner of the contract
+
+        @param _fromToken Token to be sold in exchange for `baseToken`
+        @param _start Initial offer of `fromToken` for the requested `_amount` of base,
+            should be below the market reference
+        @param _ref Reference or "market" offer of `fromToken` for the requested `_amount` of base,
+            it should be estimated with the current exchange rate, the real offered amount reaches
+            this value after 10 minutes
+        @param _limit Maximum amount of `fromToken` to exchange for the requested `_amount` of base,
+            after this limit is reached, the requested `_amount` starts to reduce
+        @param _amount Amount requested in exchange for `fromToken` until `_limit is reached`
+
+        @return The ID of the created auction
+    */
     function create(
         IERC20 _fromToken,
         uint256 _start,
@@ -67,13 +103,16 @@ contract CollateralAuction is ReentrancyGuard, Ownable {
         require(_start < _ref, "auction: offer should be below refence offer");
         require(_ref <= _limit, "auction: reference offer should be below or equal to limit");
 
+        // Calculate how much time takes the auction to offer all the `_limit` tokens
+        // in exchange for the requested base `_amount`, this delta defines the linear
+        // function of the first half of the auction
         uint32 limitDelta = ((_limit - _start).mult(DELTA_TO_MARKET) / (_ref - _start)).toUint32();
 
-        // Pull tokens for the auction
+        // Pull tokens for the auction, the full `_limit` is pulled
+        // any exceeding tokens will be returned at the end of the auction
         require(_fromToken.safeTransferFrom(msg.sender, address(this), _limit), "auction: error pulling _fromToken");
 
-        // Trust that the owner transfered the tokens
-        // the `_limit` should be transfered
+        // Create and store the auction
         id = auctions.push(Auction({
             fromToken: _fromToken,
             startTime: uint64(_now()),
@@ -93,6 +132,24 @@ contract CollateralAuction is ReentrancyGuard, Ownable {
         );
     }
 
+    /**
+        @notice Takes an ongoing auction, exchanging the requested `baseToken`
+            for offered `fromToken`. The `baseToken` are transfered to the owner
+            address and a callback to the owner is called for further processing of the tokens
+
+        @dev In the context of a collateral auction, the tokens are used to pay a loan.
+            If the oracle of the loan requires `oracleData`, such oracle data should be included
+            on the `_data` field
+
+        @dev The taker of the auction may request a callback to it's own address, this is
+            intended to allow the taker to use the newly received `fromToken` and perform
+            arbitrage with a dex before providing the requested `baseToken`
+
+        @param _id ID of the auction to take
+        @param _data Arbitrary data field that's passed to the owner
+        @param _callback Requests a callback for the taker of the auction,
+            that may be used to perform arbitrage
+    */
     function take(
         uint256 _id,
         bytes calldata _data,
@@ -101,8 +158,14 @@ contract CollateralAuction is ReentrancyGuard, Ownable {
         Auction memory auction = auctions[_id];
         require(auction.amount != 0, "auction: does not exists");
 
+        // Load the current rate of the auction
+        // how much `fromToken` is being sold and how much
+        // `baseToken` is requested
         (uint256 selling, uint256 requesting) = _offer(auction);
         address owner = _owner;
+
+        // Any non-offered `fromToken` is going
+        // to be returned to the owner
         uint256 leftOver = auction.limit - selling;
 
         // Delete auction entry
@@ -113,21 +176,20 @@ contract CollateralAuction is ReentrancyGuard, Ownable {
         // and for that, it needs the tokens that's going to sell
         require(auction.fromToken.safeTransfer(msg.sender, selling), "auction: error sending tokens");
 
-        // If msg.sender is a contract, we ping it so it can perform any arbitrage
-        // if such arbitrage is neccesary
+        // If a callback is requested, we ping the sender so it can perform arbitrage
         if (_callback) {
             /* solium-disable-next-line */
             (bool success, ) = msg.sender.call(abi.encodeWithSignature("onTake()"));
             require(success, "auction: error during callback onTake()");
         }
 
-        // Swap tokens for base
-        // baseToken should have already been transfered during create
-        // of the auction, that's trusted because only the owner can create auctions
+        // Swap tokens for base, send base directly to the owner
         require(baseToken.transferFrom(msg.sender, owner, requesting), "auction: error pulling tokens");
+
+        // Send any leftOver tokens
         require(auction.fromToken.safeTransfer(owner, leftOver), "auction: error sending leftover tokens");
 
-        // Callback to owner
+        // Callback to owner to process the closed auction
         CollateralAuctionCallback(owner).auctionClosed(
             _id,
             leftOver,
@@ -143,16 +205,42 @@ contract CollateralAuction is ReentrancyGuard, Ownable {
         );
     }
 
+    /**
+        @notice Calculates the current offer of an auction if it were to be taken,
+            how much `baseTokens` are being requested for how much `baseToken`
+
+        @param _id ID of the auction
+
+        @return How much is being requested and how much is being offered
+    */
     function offer(
         uint256 _id
     ) external view returns (uint256 selling, uint256 requesting) {
         return _offer(auctions[_id]);
     }
 
+    /**
+        @notice Returns the current timestamp
+
+        @dev Used for unit testing
+
+        @return The current Unix timestamp
+    */
     function _now() internal view returns (uint256) {
         return now;
     }
 
+    /**
+        @notice Calculates the current offer of an auction, with the auction
+            in memory
+
+        @dev If `fromToken` and `baseToken` are the same token, the auction
+            rate is fixed as 1:1
+
+        @param _auction Aunction in memory
+
+        @return How much is being requested and how much is being offered
+    */
     function _offer(
         Auction memory _auction
     ) private view returns (uint256, uint256) {
@@ -168,6 +256,16 @@ contract CollateralAuction is ReentrancyGuard, Ownable {
         }
     }
 
+    /**
+        @notice Calculates how much `fromToken` is being sould, within the defined `_limit`
+            the auction starts at `startOffer` and the offer it's increased linearly until
+            reaching `reference` offer (after 10 minutes). Then the linear function continues
+            until all the collateral is being offered
+
+        @param _auction Auction in memory
+
+        @return How much `fromToken` is being offered
+    */
     function _selling(
         Auction memory _auction
     ) private view returns (uint256 _amount) {
@@ -181,6 +279,20 @@ contract CollateralAuction is ReentrancyGuard, Ownable {
         }
     }
 
+    /**
+        @notice Calculates how much `baseToken` is being requested, before offering
+            all the `_limit` `fromToken` the total `_amount` of `baseToken` is requested.
+            After all the `fromToken` is being offered, the auction switches and the requested
+            `baseToken` goes down linearly, until reaching 1 after 24 hours
+
+        @dev If the auction is not taken after the requesting amount reaches 1, the second part
+            of the auction restarts and the initial amount of `baseToken` is requested, the process
+            repeats until the auction is taken
+
+        @param _auction Auction in memory
+
+        @return How much `baseToken` are being requested
+    */
     function _requesting(
         Auction memory _auction
     ) private view returns (uint256 _amount) {
