ee9696fb50074cf2dc60be3b76c38d682b2b5259
rotcivegaf <victorfage@gmail.com>
2021-06-25 16:59:19 +0200

    use 2 space in js

--- test/utils/TestSignedMath.js
+++ test/utils/TestSignedMath.js
@@ -1,25 +1,25 @@
 const TestSignedMath = artifacts.require('TestSignedMath');
 
 contract('Test BytesUtils', function ([_]) {
-    let signedMath;
+  let signedMath;
 
-    before('Create contracts', async function () {
-        signedMath = await TestSignedMath.new();
-    });
+  before('Create contracts', async function () {
+    signedMath = await TestSignedMath.new();
+  });
 
-    it('Function min', async function () {
-        assert.equal(await signedMath.min(0, 0), 0);
-        assert.equal(await signedMath.min(-1, 0), -1);
-        assert.equal(await signedMath.min(0, -1), -1);
-        assert.equal(await signedMath.min(1, 0), 0);
-        assert.equal(await signedMath.min(0, 1), 0);
-    });
+  it('Function min', async function () {
+    assert.equal(await signedMath.min(0, 0), 0);
+    assert.equal(await signedMath.min(-1, 0), -1);
+    assert.equal(await signedMath.min(0, -1), -1);
+    assert.equal(await signedMath.min(1, 0), 0);
+    assert.equal(await signedMath.min(0, 1), 0);
+  });
 
-    it('Function max', async function () {
-        assert.equal(await signedMath.max(0, 0), 0);
-        assert.equal(await signedMath.max(-1, 0), 0);
-        assert.equal(await signedMath.max(0, -1), 0);
-        assert.equal(await signedMath.max(1, 0), 1);
-        assert.equal(await signedMath.max(0, 1), 1);
-    });
+  it('Function max', async function () {
+    assert.equal(await signedMath.max(0, 0), 0);
+    assert.equal(await signedMath.max(-1, 0), 0);
+    assert.equal(await signedMath.max(0, -1), 0);
+    assert.equal(await signedMath.max(1, 0), 1);
+    assert.equal(await signedMath.max(0, 1), 1);
+  });
 });
